8e4f14ca9c5607076c2b53e3201ada3d869c96a6
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-10-28 20:45:10 -0400

    More movement work

--- scripts/TimeTrials/player.gd
+++ scripts/TimeTrials/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 const mouse_sensitivity = 0.002
 
-const sprint_speed = 25 # running speed
-const move_speed = 20 #not running speed
+const sprint_speed = 15 # running speed
+const move_speed = 10 #not running speed
 
 const air_sprint_speed = 25 # running speed
 const air_move_speed = 20 #not running speed
@@ -24,9 +24,17 @@ func _unhandled_input(event):
 		$CameraAnchor/Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$CameraAnchor/Camera3D.rotation.x = clampf($CameraAnchor/Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
+func update_velocity_feed() -> void:
+	var velX = velocity.x
+	var velY = velocity.y
+	var velZ = velocity.z
+	$CameraAnchor/Camera3D/VelocityDebug.text = "VelX:"+str(velX)+" VelY:"+str(velY)+" VelZ:"+str(velZ)
+
 func _physics_process(delta: float) -> void:
+	update_velocity_feed()
+	
 	var applied_speed = move_speed
-	target_velocity = Vector3.ZERO
+	#target_velocity = Vector3.ZERO
 	world_target_velocity = Vector3.ZERO
 	
 	
@@ -39,13 +47,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		applied_speed = 2 #m/s
 	
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_key_pressed(KEY_W) and abs(velocity.z) < applied_speed:
 		target_velocity.z -= applied_speed
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(KEY_S) and abs(velocity.z) < applied_speed:
 		target_velocity.z += applied_speed
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A) and abs(velocity.x) < applied_speed:
 		target_velocity.x -= applied_speed
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_D) and abs(velocity.x) < applied_speed:
 		target_velocity.x += applied_speed
 	
 	
@@ -56,9 +64,11 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor():
 			world_target_velocity.x = lerpf(target_velocity.x,0,0.5)
 			world_target_velocity.y = lerpf(target_velocity.y,0,0.5)
-			
-	velocity.x = world_target_velocity.x
-	velocity.z = world_target_velocity.z
+		
+	if abs(velocity.x) < applied_speed:
+		velocity.x += world_target_velocity.x * delta
+	if abs(velocity.z) < applied_speed:
+		velocity.z += world_target_velocity.z * delta
 	
 	velocity.y -= gravity * delta
 	move_and_slide()
